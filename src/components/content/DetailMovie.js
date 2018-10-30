6eed3deefa4d4382eb9bf22135775d92c3777b62
kai <khai98yn@gmail.com>
2018-10-30 23:29:34 +0700

    version 0.3: fix share fb bug!

--- src/components/content/DetailMovie.js
+++ src/components/content/DetailMovie.js
@@ -76,7 +76,7 @@ class DetailMovie extends Component {
                                     <FontAwesomeIcon icon={['fab', 'facebook']}/> Share</button> */}
                                     <div className="fb-share-button" data-href={`https://marvelfan.herokuapp.com/movies/${this.props.match.params.moviesId}`} data-layout="button" data-size="large" data-mobile-iframe="true">
                                     {/* <div className="fb-share-button" data-href="https://eslint.org/docs/rules/no-useless-concat" data-layout="button" data-size="large" data-mobile-iframe="true"> */}
-                                        <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A3000%2Fmovies%2F335983&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a>
+                                        <a target="_blank" rel="noopener noreferrer" href={`https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fmarvelfan.herokuapp.com%2Fmovies%2F${this.props.match.params.moviesId}&amp;src=sdkpreparse`} className="fb-xfbml-parse-ignore">Share</a>
                                     </div>
                             </div>    
                             <div className="col-half" style={{width: this.state.colhalfWidth}}>
--- src/components/content/DetailTVshow.js
+++ src/components/content/DetailTVshow.js
@@ -73,7 +73,7 @@ class DetailTVshow extends Component {
                                     <FontAwesomeIcon icon={['fab', 'facebook']}/> Share</button> */}
                                 <div className="fb-share-button" data-href={`https://marvelfan.herokuapp.com/tvshows/${this.props.match.params.tvshowId}`} data-layout="button" data-size="large" data-mobile-iframe="true">
                                 {/* <div className="fb-share-button" data-href="https://dashboard.heroku.com/apps/marvelpj-server" data-layout="button" data-size="large" data-mobile-iframe="true"> */}
-                                    <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A3000%2Fmovies%2F335983&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a>
+                                    <a target="_blank" rel="noopener noreferrer" href={`https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fmarvelfan.herokuapp.com%2Ftvshows%2F${this.props.match.params.tvshowId}&amp;src=sdkpreparse`} className="fb-xfbml-parse-ignore">Share</a>
                                 </div>
                             </div>
                             <div className="col-half" style={{width: this.state.colhalfWidth}}>
